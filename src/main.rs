a8ff95fd460aedaa513d5e661ddfc5288f534bd9
funnyboy-roks <funnyboyroks@gmail.com>
2025-08-07 03:50:44 -0500

    add set px param

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use std::{net::SocketAddr, sync::Arc};
+use std::{net::SocketAddr, sync::Arc, time::Duration};
 
 use anyhow::{bail, Context};
 use dashmap::DashMap;
 use tokio::{
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
     net::{TcpListener, TcpStream},
+    time::Instant,
 };
 
 pub mod resp;
 
+#[derive(Debug, Clone)]
+struct MapValue {
+    value: String,
+    expires_at: Option<Instant>,
+}
+
+#[derive(Debug, Clone)]
 struct State {
-    map: DashMap<String, String>,
+    map: DashMap<String, MapValue>,
 }
 
 async fn handle_connection(
@@ -55,6 +63,19 @@ async fn handle_connection(
                 let key = command[1].clone();
                 let value = command[2].clone();
 
+                let value = MapValue {
+                    value,
+                    expires_at: if command.len() > 4 && command[3].eq_ignore_ascii_case("px") {
+                        let ms: u64 = command[4]
+                            .parse()
+                            .context("parsing millis until expiration")?;
+
+                        Some(Instant::now() + Duration::from_millis(ms))
+                    } else {
+                        None
+                    },
+                };
+
                 state.map.insert(key, value);
 
                 resp::write(&mut tx, serde_json::json!("OK"))
@@ -64,8 +85,17 @@ async fn handle_connection(
             "get" => {
                 let key = &command[1];
                 let value = if let Some(value) = state.map.get(key) {
-                    serde_json::Value::String(value.clone())
+                    if value.expires_at.is_none_or(|e| Instant::now() < e) {
+                        eprintln!("get {key} from map -> {}", value.value);
+                        serde_json::Value::String(value.value.clone())
+                    } else {
+                        drop(value);
+                        state.map.remove(key);
+                        eprintln!("remove {key} from map because expired");
+                        serde_json::Value::Null
+                    }
                 } else {
+                    eprintln!("get {key} from map -> (nil)");
                     serde_json::Value::Null
                 };
 
