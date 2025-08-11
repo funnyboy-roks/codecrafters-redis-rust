8a86f41db8554385c181da952d8933aa17602b98
funnyboy-roks <funnyboyroks@gmail.com>
2025-08-11 01:31:24 -0500

    replica -> slave and ensure "replication"

--- src/command/replication.rs
+++ src/command/replication.rs
@@ -1,5 +1,14 @@
+use anyhow::{bail, ensure};
+
 use crate::{resp::Value, State};
 
 pub async fn info(state: &State, args: &[String]) -> anyhow::Result<Value> {
+    let [section, ..] = args else {
+        bail!("TODO: args.len() < 1");
+    };
+    ensure!(
+        section == "replication",
+        "Section {section} is not implemented."
+    );
     Ok(Value::from(format!("role:{}", state.role)))
 }
--- src/main.rs
+++ src/main.rs
@@ -57,7 +57,7 @@ impl Display for Role {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Role::Master => write!(f, "master"),
-            Role::Replica(_) => write!(f, "replica"),
+            Role::Replica(_) => write!(f, "slave"),
         }
     }
 }
